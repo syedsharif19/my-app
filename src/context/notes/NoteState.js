03e4a52e25a848811f833afed5b7dd2c9186b10e
syedsharif19 <143947875+syedsharif19@users.noreply.github.com>
2024-10-13 17:47:07 +0530

    Update NoteState.js

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,7 +3,7 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 function NoteState(props) {
-  const host = "http://localhost:3000";
+  const host = "https://backend22.netlify.app/";
   const notesInitial = []
   const [notes, setNotes] = useState(notesInitial)
 
