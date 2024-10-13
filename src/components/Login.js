c0f065aeb9138018b406560da03c4799bc284b7c
syedsharif19 <143947875+syedsharif19@users.noreply.github.com>
2024-10-13 17:44:48 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,7 +14,7 @@ const Login = (props) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:3000/api/auth/login", {
+    const response = await fetch("https://backend22.netlify.app//api/auth/login", {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
